1da331cf44c3c592160352a85435d2a69ce12ea3
bwielk <bwielk@gmail.com>
2018-02-24 17:33:31 +0100

    Dog extends the Animal abstract class

---
+++ src/Dog.java
@@ -0,0 +1,15 @@
+
+public class Dog extends Animal{
+
+	public Dog(String name) {
+		super(name);
+	}
+	
+	public void eat(){
+		System.out.println(getName() + "is eating");
+	}
+	
+	public void breathe(){
+		System.out.println(getName() + "is breathing");
+	}
+}
